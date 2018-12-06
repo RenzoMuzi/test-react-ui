78292b6f3136f2482ab3806b851cfba1dc067271
Renzo Muzi <renzo.muzi@moove-it.com>
2018-12-06 13:26:31 -0300

    install Rollupjs

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, ''),
-    publicPath: '/',
+    publicPath: '',
     filename: '[name]',
     library: 'react-ui',
     libraryTarget: 'umd',
